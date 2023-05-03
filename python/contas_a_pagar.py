829fafa035a649163741b7a3b06f392ca4e6adc8
Caio Sergio <csrmyo@gmail.com>
2023-05-03 14:25:50 -0300

    contas_a_pagar pandas copy error log fixed

--- python/contas_a_pagar.py
+++ python/contas_a_pagar.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from datetime import datetime, timedelta
+from pprint import pprint as pp
+
+pd.options.display.float_format = "{:,.2f}".format
 
 class Resumo:
     def __init__(self, list_of_dividas, money=0):
@@ -20,15 +23,16 @@ class Resumo:
         self.df.sort_values(by="vencimento", inplace=True)
         self.df.reset_index(drop=True, inplace=True)
         self.d = self.df[self.df["vencimento"] < self.period]
-        self.d["proportions"] = [self.d["valor"][i] / self.d["valor"].sum() for i in self.d.index]
-        self.d["restante"] = [(self.d["valor"].sum() + self.money) * self.d["proportions"][i] for i in self.d.index]
-        self.d["restante_pdia"] = [(self.d["valor"].sum() + self.money) * self.d["proportions"][i] / self.d["dias ateh"][i] for i in self.d.index]
-        self.d["cumulativo"] = [self.d["restante_pdia"][:i+1].sum() for i in self.d.index]
-
+        self.d = self.d.assign(proportions=self.d["valor"] / self.d["valor"].sum())
+        self.d = self.d.assign(restante=(self.d["valor"].sum() + self.money)*self.d["proportions"])
+        self.d = self.d.assign(restante_pdia=(self.d["valor"].sum() + self.money)*self.d["proportions"]/self.d["dias ateh"])
+        self.d = self.d.assign(cumulativo=[self.d.loc[:i+1, "restante_pdia"].sum() for i in self.d.index])
+        self.d = self.d.assign(valor_cumulativo=[self.d.loc[:i+1, "valor"].sum() for i in self.d.index])
+        
         self.restante_valor = self.d["restante"].sum()
         self.restante_pdia = self.d["restante_pdia"].sum()
-        self.total_pdia = self.df["por_dia"].sum()
-        self.total_valor = self.df["valor"].sum()
+        self.total_pdia = self.d["por_dia"].sum()
+        self.total_valor = self.d["valor"].sum()
 
 
         
@@ -67,6 +71,12 @@ class Divida:
             "vencimento": self.vencimentos,
             "por_dia": self.values_per_day,
             "dias ateh": self.days_until})
+        
+    # def __str__(self):
+    #     return f"{self.description}: {self.installments}x{self.value} ({self.total:.2})"
+    
+    def __repr__(self):
+        return f"{self.description}: {self.installments}x{self.value} ({float(self.total):,.2f})"
 
 
 dividas = [
@@ -85,9 +95,11 @@ dividas = [
 # for x in dividas:
 #     print(x.df)
 
-z = Resumo(dividas, money=1215.22)
+z = Resumo(dividas, money=2003.38)
 
 print(z.df)
+z.set_period(m=6)
+z.a()
 
 
 
